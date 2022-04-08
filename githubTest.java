671bc7ce6a29312aa58e127f946ec11f89e6c00b
tgero8994 <tgero8994@students.mc3.edu>
2022-04-08 17:50:16 -0400

    Added for loop that prints out the numbers 1 - 100
    For loop prints out each number from 1 to 100

--- githubTest.java
+++ githubTest.java
@@ -3,5 +3,8 @@ public class githubTest
    public static void main(String[] agrs)
    {
       System.out.println("Hello world!");
+      
+      for(int i =0; i<=100; i++)
+         System.out.println(i);
    }
 }
